e41c534ae931aba89179795eb0c13f35f33b458e
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2012-09-12 19:40:32 -0300

    Adicionado requirements e base para deploy no heroku

--- pylestras/settings.py
+++ pylestras/settings.py
@@ -1,4 +1,5 @@
 # Django settings for pylestras project.
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,14 +11,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='postgres://localhost')
 }
 
 # Local time zone for this installation. Choices can be found here:
