d61222647f76ed4f0fac6a8173c2cc2dfb8ae102
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2012-09-12 19:43:23 -0300

    Adicionado gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pylestras.wsgi -b 0.0.0.0:$PORT
--- pylestras/settings.py
+++ pylestras/settings.py
@@ -113,6 +113,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
