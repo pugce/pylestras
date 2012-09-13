784873c6d94732a83fa5f871abb95ef5d01a6b27
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2012-09-13 16:38:29 -0300

    Servindo arquivos estáticos via Django
    Corrigir isso no Heroku

--- pylestras/urls.py
+++ pylestras/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,3 +8,6 @@ urlpatterns = patterns('',
     url(r'', include('eventos.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# TODO: definir como servir arquivos estaticos no heroku
+urlpatterns += staticfiles_urlpatterns()
