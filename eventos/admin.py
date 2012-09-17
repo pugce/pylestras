35730351ec90680643a47acd2eb6ada05af4f030
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2012-09-17 16:55:01 -0300

    Adicionado novamente admin para o model Palestra

--- eventos/admin.py
+++ eventos/admin.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 from eventos.models import (Evento, Profile, Inscricao,
                             Palestra, Realizacao, Patrocinio)
 
+
 class PalestraInline(admin.TabularInline):
     model = Palestra
-    prepopulated_fields = { 'slug': ('titulo',)}
+    prepopulated_fields = {'slug': ('titulo',)}
     extra = 1
 
 
@@ -22,10 +23,9 @@ class EventoAdmin(admin.ModelAdmin):
         ('Patrocinadores',               {'fields': ['patrocinadores'], 'classes': ['collapse']}),
     ]
     inlines = [PalestraInline]
-
     list_display = ('titulo', 'data_realizacao', 'publicado')
     list_filter = ['data_realizacao']
-    search_fields = ['titulo'] # pode-se colocar mais de um, porém MAKE IT SIMPLE
+    search_fields = ['titulo']  # pode-se colocar mais de um, porém MAKE IT SIMPLE
     date_hierarchy = 'data_realizacao'
 
 
@@ -37,8 +37,15 @@ class PatrocinioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'contato', 'tipo')
 
 
+class PalestraAdmin(admin.ModelAdmin):
+    list_display = ('titulo',)
+    prepopulated_fields = {'slug': ('titulo',)}
+    search_fields = ['titulo']
+
+
 admin.site.register(Evento, EventoAdmin)
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Patrocinio, PatrocinioAdmin)
+admin.site.register(Palestra, PalestraAdmin)
 admin.site.register(Inscricao)
 admin.site.register(Realizacao)
