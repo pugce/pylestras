7c9af73fc0dc69c1ab92e8023d7d32f2ad184805
Mario Chaves <macndesign@gmail.com>
2012-09-30 14:26:56 -0300

    layout responsivo para as logos em diversas dimensoes com versao da pylestra dinamica (atraves do context_processor) + consertos de bugs ao redimensionar o navegador.

--- eventos/context_processors.py
+++ eventos/context_processors.py
@@ -2,6 +2,9 @@ from eventos.models import Evento
 
 
 def eventos(request):
+    evento_list = Evento.publicados.all()
+    ultima_versao = str(evento_list.latest())[0]
     return {
-        'eventos': Evento.publicados.all(),
+        'eventos': evento_list,
+        'ultima_versao': ultima_versao,
     }
