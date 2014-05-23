37f1260224ed307442860f79115116e7f4a8a2b2
Fábio Cerqueira <fabio@insert.uece.br>
2014-05-23 14:26:51 -0300

    Pequena correção na versão do logo evento

--- eventos/context_processors.py
+++ eventos/context_processors.py
@@ -3,7 +3,7 @@ from eventos.models import Evento
 
 def eventos(request):
     evento_list = Evento.publicados.all()
-    ultima_versao = str(evento_list.latest())[0]
+    ultima_versao = evento_list.latest().titulo.split()[0]
     return {
         'eventos': evento_list,
         'ultima_versao': ultima_versao,
